7cab7f25f0cc2bd2255eabb5c89746e669f3c4f3
Lassi <lassi.kuisma1@gmail.com>
2018-05-04 00:31:01 +0300

    Incomplete Youtube stuff...

---
+++ ytapi/java/YoutubeDownloader.java
@@ -0,0 +1,38 @@
+import com.sapher.youtubedl.YoutubeDL;
+import com.sapher.youtubedl.YoutubeDLException;
+import com.sapher.youtubedl.YoutubeDLRequest;
+import com.sapher.youtubedl.YoutubeDLResponse;
+
+public class YoutubeDownloader {
+
+    public static final String OUTPUT_FOLDER = "mumbot_downloads";
+
+    public static void download(String videoUrl, String outputName) {
+        // Video url to download
+        //String videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        //YoutubeDL.setExecutablePath("C:/Users/Lassi_2/IdeaProjects/mumbot/youtube-dl.exe");
+
+
+        // Build request
+        YoutubeDLRequest request = new YoutubeDLRequest(videoUrl);
+        //request.setOption("ignore-errors");		// --ignore-errors
+        //request.setOption("output", "audio_test_ba.mp4");
+        request.setOption("output", "audio_test_ba.mp4");
+        request.setOption("retries", 10);
+        request.setOption("extract-audio");
+        request.setOption("audio-format", "mp3");
+        request.setOption("format", "bestaudio");
+
+        // Make request and return response
+        try {
+            YoutubeDLResponse response = YoutubeDL.execute(request);
+
+            String stdOut = response.getOut();
+            System.out.println("Response: " + stdOut);
+        } catch (YoutubeDLException e) {
+            System.err.println("Something went wrong.");
+            e.printStackTrace();
+        }
+    }
+
+}
